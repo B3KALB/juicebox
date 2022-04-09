f43bfff0acb8e2ba1d196ad12f33e0d94cdcd6fb
B3KALB <B3KALB@gmail.com>
2022-04-09 18:50:01 -0500

    april9KelseyGotMyBack

--- api/tags.js
+++ api/tags.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const tagsRouter = express.Router();
-const { getAllTags } = require('../db');
+const { getAllTags, getPostsByTagName } = require('../db');
 
 tagsRouter.use((req, res, next) => {
   console.log("A request is being made to /tags");
@@ -8,11 +8,18 @@ tagsRouter.use((req, res, next) => {
   next(); // THIS IS DIFFERENT
 });
 
-tagsRouter.get('/', async (req, res, next) => {
-    const users = await getAllTags();
-  
-    res.send(users);
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+  try {
+      const {tagName} = req.params;
+      const users = await getPostsByTagName(tagName);
+      console.log(users)
+      // use our method to get posts by tag name from the db
+      // send out an object to the client { posts: // the posts }
+      res.send(users)
+    } catch ({ name, message }) {
+      // forward the name and message to the error handler
+      next({ name, message })
+    }
   });
-
 
 module.exports = tagsRouter;
