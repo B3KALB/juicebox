36ed74962d622e3009afb0866f757ce0342c5bd8
B3KALB <B3KALB@gmail.com>
2022-04-09 12:18:54 -0500

    april9ThroughPatch

--- api/posts.js
+++ api/posts.js
@@ -3,6 +3,41 @@ const postsRouter = express.Router();
 const { getAllPosts, createPost } = require('../db');
 const { requireUser } = require('./utils');
 
+postsRouter.patch('/:postId', requireUser, async (req, res, next) => {
+  const { postId } = req.params;
+  const { title, content, tags } = req.body;
+
+  const updateFields = {};
+
+  if (tags && tags.length > 0) {
+    updateFields.tags = tags.trim().split(/\s+/);
+  }
+
+  if (title) {
+    updateFields.title = title;
+  }
+
+  if (content) {
+    updateFields.content = content;
+  }
+
+  try {
+    const originalPost = await getPostById(postId);
+
+    if (originalPost.author.id === req.user.id) {
+      const updatedPost = await updatePost(postId, updateFields);
+      res.send({ post: updatedPost })
+    } else {
+      next({
+        name: 'UnauthorizedUserError',
+        message: 'You cannot update a post that is not yours'
+      })
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
 postsRouter.post('/', requireUser, async (req, res, next) => {
   const { title, content, tags = "" } = req.body;
 
--- index.js
+++ index.js
@@ -35,7 +35,22 @@ server.use((req, res, next) => {
     console.log("<_____Body Logger END_____>");
   
     next();
-  });
+});
+
+server.get('/background/:color', (req, res, next) => {
+  res.send(`
+    <body style="background: ${ req.params.color };">
+      <h1>Hello World</h1>
+    </body>
+  `);
+});
+
+server.get('/add/:first/to/:second', (req, res, next) => {
+  res.send(`<h1>${ req.params.first } + ${ req.params.second } = ${
+    Number(req.params.first) + Number(req.params.second)
+   }</h1>`);
+});
+
 // stuff below here
 const PORT = 3000;
 server.listen(PORT, () => {
