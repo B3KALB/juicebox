619b0c963a030997648f7e986af4b69b68d29de0
B3KALB <B3KALB@gmail.com>
2022-04-10 14:59:05 -0500

    april10DeleteIsAdded

--- db/index.js
+++ db/index.js
@@ -196,7 +196,14 @@ async function updatePost(postId, fields = {}) {
         FROM posts
         WHERE id=$1;
       `, [postId]);
-  
+      
+      if (!post) {
+        throw {
+          name: "PostNotFoundError",
+          message: "Could not find a post with that postId"
+        };
+      }
+
       const { rows: tags } = await client.query(`
         SELECT tags.*
         FROM tags
@@ -346,6 +353,7 @@ async function updatePost(postId, fields = {}) {
     getAllPosts,
     getPostsByUser,
     getPostsByTagName,
+    getPostById,
     createTags,
     getAllTags,
     getUserByUsername,
